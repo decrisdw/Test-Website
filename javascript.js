079eb80f51496dc5ebcd12512299f93eeaa14022
decrisdw <deven.decristofaro@gmail.com>
2017-12-20 22:28:22 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,35 +1,50 @@
+
 $(document).ready(function(){
 	
+	// Puts the text field IDs into an array and clears their fields on page load
+	var text_field_IDs = ["bench_input_text", "squat_input_text", "deadlift_input_text"];
+	
+	for (i = 0; i < text_field_IDs.length; i++) {	
+		document.getElementById(text_field_IDs[i]).value = "";	
+	}
+	
 	// Sets the example text
-	$('.bench_weight_text').text('300');
+	//$('.bench_weight_text').text('300');
 	//$('.squat_weight_text').text('350');
-	$('.deadlift_weight_text').text('450');
+	//$('.deadlift_weight_text').text('450');
 
-	// Checks if the squat checkbox is checked
-	$('#squat_checkbox').prop('checked', false);
-	$('#squat_checkbox').change(function() {
-		if($(this).is(':checked')) {
-			$('.squat_inputs_container').css({'display':'block'});
+	// Checks if a lift checkbox is checked
+	$('.checkbox_label input').prop('checked', false);
+	
+	$('.checkbox_label input').change(function() {
+		var input_ID = $(this).attr('id');
+		var lift_string = input_ID.substr(0, input_ID.indexOf('_'));
+		
+		if($(this).is(':checked')) {		
+			$('#'+lift_string+'_inputs_container').css({'display':'block'});
 		}
 		else {
-			$('.squat_inputs_container').css({'display':'none'});
+			$('#'+lift_string+'_inputs_container').css({'display':'none'});
 		}
 	});
 	
 	// Handles the submit button
-	$('#squat_input_button').submit(function( event ) {
+	$('.lift_form').submit(function( event ) {
+		
+		var submitted_ID = $(this).attr('id');
+		var submitted_string = submitted_ID.substr(0, submitted_ID.indexOf('_'));
 		
 		// Get the inputted squat value
-		var squatInput = $('#squat_input_text').val();
+		var lift_input = $('#' + submitted_ID + ' .form-control').val();
 		
 		// Set the inputted squat value
-		$('.squat_weight_text').text(squatInput);
+		$('.' + submitted_string + '_weight_text').text(lift_input);
 		
 		event.preventDefault();
 		
 	});
 	
-	$('#squat_input_text').keypress(function (e) {
+	/**$('#squat_input_text').keypress(function (e) {
 		//if the letter is not digit then display error and don't type anything
 		if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
 
@@ -38,6 +53,6 @@ $(document).ready(function(){
 
 			return false;
 		}
-	});
+	**/
 	
 }); 
